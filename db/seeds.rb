55b3a0e4aebb4c95df9d89bd72b98981e10400a9
leggomyeggo96 <ethantong96@gmail.com>
2026-02-02 20:57:02 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create!([
+  {
+    title: "The Life of a Turtle",
+    author: "Ethan Tong",
+    price: 19.99,
+    published_date: "2020-01-01"
+  },
+  {
+    title: "The Mirror, the Lion and the Audacity of the Little Dog",
+    author: "C.S. Something",
+    price: 14.50,
+    published_date: "2018-05-10"
+  },
+  {
+    title: "Origami for Cats",
+    author: "Cat Master",
+    price: 9.99,
+    published_date: "2021-03-15"
+  },
+  {
+    title: "The Weight of a Hundred Kilograms",
+    author: "Gym Bro",
+    price: 24.99,
+    published_date: "2019-07-21"
+  },
+  {
+    title: "Rails Deployment Nightmares",
+    author: "You",
+    price: 29.99,
+    published_date: "2026-02-02"
+  }
+])
