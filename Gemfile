9947a02fdafdd72651b0afa4167a37a4d37ce175
leggomyeggo96 <ethantong96@gmail.com>
2026-02-06 11:41:03 -0600

    initial commit from book collection 4

--- Gemfile
+++ Gemfile
@@ -31,7 +31,6 @@ gem "solid_cable"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
-gem "rexml"
 
 # Deploy this application anywhere as a Docker container [https://kamal-deploy.org]
 gem "kamal", require: false
@@ -46,12 +45,11 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
 
-  gem "rspec-rails"
-
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
+  gem "rubocop-rails-omakase", require: false
 end
 
 group :development do
@@ -65,5 +63,4 @@ group :test do
   gem "selenium-webdriver"
 end
 
-gem "brakeman"
-gem "rubocop"
+gem "rspec-rails", "~> 8.0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,9 @@
-Book.destroy_all
-
-if Rails.env.development?
-  Book.create!([
-    { title: "DEV Book A", author: "Dev", price: 10.99, published_date: "2020-01-01" },
-    { title: "DEV Book B", author: "Dev", price: 10.99, published_date: "2020-01-01" },
-    { title: "DEV Book C", author: "Dev", price: 10.99, published_date: "2020-01-01" },
-    { title: "DEV Book D", author: "Dev", price: 10.99, published_date: "2020-01-01" },
-    { title: "DEV Book E", author: "Dev", price: 10.99, published_date: "2020-01-01" },
-    { title: "DEV Book F", author: "Dev", price: 10.99, published_date: "2020-01-01" },
-    { title: "DEV Book G", author: "Dev", price: 10.99, published_date: "2020-01-01" }
-  ])
-
-elsif Rails.env.test?
-  Book.create!([
-    { title: "TEST Book 1", author: "Test", price: 10.99, published_date: "2020-01-01" },
-    { title: "TEST Book 2", author: "Test", price: 10.99, published_date: "2020-01-01" },
-    { title: "TEST Book 3", author: "Test", price: 10.99, published_date: "2020-01-01" },
-    { title: "TEST Book 4", author: "Test", price: 10.99, published_date: "2020-01-01" },
-    { title: "TEST Book 5", author: "Test", price: 10.99, published_date: "2020-01-01" }
-  ])
-
-elsif Rails.env.production?
-  Book.create!([
-    { title: "PROD ONLY Book X", author: "Prod", price: 10.99, published_date: "2020-01-01" },
-    { title: "PROD ONLY Book Y", author: "Prod", price: 10.99, published_date: "2020-01-01" }
-  ])
-end
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
